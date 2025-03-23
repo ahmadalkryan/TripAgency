f0ae5eb9565e1f74ed478da3a31c017f8eb9f025
AlaaZreekie <alaasama282@gmail.com>
2025-03-23 14:19:18 +0300

    SaveAll

---
+++ TripAgency/DataAccessLayer/Migrations/20250323111759_Blank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Blank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_date_time",
+                table: "Bookings",
+                type: "datetime2(7)",
+                nullable: false,
+                defaultValue: new DateTime(2025, 3, 23, 14, 17, 58, 802, DateTimeKind.Local).AddTicks(7062),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(7)",
+                oldDefaultValue: new DateTime(2025, 3, 23, 11, 17, 4, 139, DateTimeKind.Local).AddTicks(8096));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_date_time",
+                table: "Bookings",
+                type: "datetime2(7)",
+                nullable: false,
+                defaultValue: new DateTime(2025, 3, 23, 11, 17, 4, 139, DateTimeKind.Local).AddTicks(8096),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(7)",
+                oldDefaultValue: new DateTime(2025, 3, 23, 14, 17, 58, 802, DateTimeKind.Local).AddTicks(7062));
+        }
+    }
+}
